8845063540260ccc01e9915f63889c14fecbd845
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-04-07 19:34:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,7 @@
 window.APP_CONFIG = {
   siteName: 'ICL 會員點數中心',
-  // GitHub Pages 前端會用到
   supabaseUrl: 'https://uxqrrhmzdvdstfsfopky.supabase.co',
   supabaseAnonKey: 'sb_publishable_GmleewX4CqVQdyOz8Xlu7A_mNPCcJjB',
-
-  // LINE LIFF
   liffId: '2009696707-5igDm7EE',
-
-  // Edge Function 名稱
   apiFunctionName: 'line-api'
 };
